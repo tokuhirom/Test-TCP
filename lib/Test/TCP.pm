4b7b50af9367d4c3ea240da7055f77d41fbf8ca7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-03-26 14:02:58 +0900

    Checking in changes prior to tagging of version 0.04.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 99ac77f..6623307 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Test::TCP
    
    +0.04
    +
    +    - Test::SharedFork 0.03 has a bug. depend to 0.04
    +
     0.03
    
         - do not depend to Sub::Exporter

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Params::Validate ':all';
