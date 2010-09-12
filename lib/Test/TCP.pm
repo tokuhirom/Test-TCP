7b514253e8b879daa4fc13617dbc3f57bba878ab
tokuhirom <tokuhirom@gmail.com>
2010-09-12 12:44:47 +0900

    Checking in changes prior to tagging of version 1.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4ce43e5..3259726 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Test::TCP
    
    +1.06
    +
    +    - depend to Test::SharedFork 0.14. It fixes issue with Test::Builder2.
    +
     1.05
    
         - workaround for $@ issue in test case.

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
