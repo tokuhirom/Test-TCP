c6f82a7ee1073e413f0acc177dcf0e77aa53f61e
tokuhirom <tokuhirom@gmail.com>
2010-12-17 12:30:05 +0900

    Checking in changes prior to tagging of version 1.07_01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 099b2eb..89c0abc 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Test::TCP
    
    +1.07_01
    +
    +    - new OO interface!
    +
     1.07
    
         - allow forking in the client(lestrrat)

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '1.07';
+our $VERSION = '1.07_01';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
