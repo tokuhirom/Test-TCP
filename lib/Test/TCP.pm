7063240b116ddc34fe9a70bfa23968fa0c56de03
tokuhirom <tokuhirom@gmail.com>
2010-12-19 11:41:57 +0900

    Checking in changes prior to tagging of version 1.10.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e562422..7379c3e 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Test::TCP
    
    +1.10
    +
    +    - added Test::TCP->stop method
    +
     1.09
    
         - added document in FAQ section

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '1.09';
+our $VERSION = '1.10';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
