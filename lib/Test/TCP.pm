65f754f339b358cd201e7c38fb5c325f8c9e9d1b
tokuhirom <tokuhirom@gmail.com>
2013-03-02 18:33:34 +0900

    Checking in changes prior to tagging of version 1.20.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 17b33c3..1288149 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Test::TCP
    
    +1.20 2013-03-02T18:30:25
    +
    +    - bump up version
    +
     1.19 2013-02-27T14:02:13
    
         - Split empty_port() function to Net::EmptyPort

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '1.19';
+our $VERSION = '1.20';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
