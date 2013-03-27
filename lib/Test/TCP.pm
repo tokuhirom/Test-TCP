087f5cf0b098f9668520d5dd82dc27c4fcfd4f1e
tokuhirom <tokuhirom@gmail.com>
2013-03-27 10:37:28 +0900

    Checking in changes prior to tagging of version 1.23.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bdabd96..ccdba46 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Test::TCP
    
     {{$NEXT}}
    
    +1.23 2013-03-27T01:37:08Z
    +
    +    - Just re-packaging
    +
     1.22 2013-03-27T01:17:38Z
    
         - Add doc for Test::TCP::CheckPort

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '1.22';
+our $VERSION = '1.23';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
