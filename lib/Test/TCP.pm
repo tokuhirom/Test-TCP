9b3e929fa3b73961ed3b9469b1356ff39d9188b8
tokuhirom <tokuhirom@gmail.com>
2013-05-29 10:13:19 +0900

    Checking in changes prior to tagging of version 1.29.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 92715fb..9248f67 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Test::TCP
    
     {{$NEXT}}
    
    +1.29 2013-05-29T01:12:47Z
    +
    +    - Care the `test_tcp(port => undef)` case.
    +      https://github.com/tokuhirom/Test-TCP/issues/13
    +
     1.28 2013-05-28T11:14:27Z
    
         - Added wait_port_sleep and wait_port_retry option for

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '1.28';
+our $VERSION = '1.29';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
