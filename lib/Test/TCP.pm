cc85c59f8e1256affb1e8828e0d01b4d9af436d8
tokuhirom <tokuhirom@gmail.com>
2013-09-22 13:23:27 +0900

    Checking in changes prior to tagging of version 2.01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6c8d4d6..89562fa 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,17 @@ Revision history for Perl extension Test::TCP
    
     {{$NEXT}}
    
    +2.01 2013-09-22T04:13:53Z
    +
    +    commit 95cd6a0669d422e10f2d3b09f35346c86f7e3257
    +    Author: Christian Walde <walde.christian@googlemail.com>
    +    Date:   Sat Sep 21 16:19:49 2013 +0200
    +
    +        test waitport argument by running full code-chain, not with partial mocking
    +
    +        The partial mocks cause stuck forks on win32 at times, causing the test to
    +        hang. With the full code chain present the test runs reliably
    +
     2.00 2013-06-11T04:41:43Z
    
         [INCOMPATIBLE CHANGES]

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '2.00';
+our $VERSION = '2.01';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
