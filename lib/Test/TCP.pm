68bae61fbec309da70f629646d8c15ea223841a7
tokuhirom <tokuhirom@gmail.com>
2013-10-30 12:26:08 +0900

    Checking in changes prior to tagging of version 2.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 89562fa..e4a3c01 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Test::TCP
    
     {{$NEXT}}
    
    +2.02 2013-10-30T03:22:39Z
    +
    +    - Fixed fork(2) error handling.
    +      (tokuhirom)
    +
     2.01 2013-09-22T04:13:53Z
    
         commit 95cd6a0669d422e10f2d3b09f35346c86f7e3257

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '2.01';
+our $VERSION = '2.02';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
