f3717dbe236e11b0a8f8aa4a339c20b34c050353
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-06-24 08:47:25 +0900

    Checking in changes prior to tagging of version 2.04.
    Changelog diff is:
    
    diff --git Changes Changes
    index b11a93e..cd23914 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,10 @@ Revision history for Perl module Test::TCP
    
     {{$NEXT}}
    
    +2.04 2014-06-23T23:42:28Z
    +
    +    - Release.
    +
     2.03_02 2014-06-23T23:37:07Z
    
         - Release to CPAN.

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '2.03_02';
+our $VERSION = '2.04';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
