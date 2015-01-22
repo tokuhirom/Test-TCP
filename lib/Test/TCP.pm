a858127d096b6349cfde2b785304dba76e773d56
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-01-22 19:45:28 +0900

    Checking in changes prior to tagging of version 2.07.
    Changelog diff is:
    
    diff --git Changes Changes
    index 9bb5a67..5a688d6 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,13 @@ Revision history for Perl module Test::TCP
    
     {{$NEXT}}
    
    +2.07 2015-01-22T10:44:21Z
    +
    +    Old versions of Test::SharedFork are incompatibles with the new Test::Builder: I had "missing TB2::History" errors.
    +    It would be helpful to upgrade the Test::SharedFork dependency to help the user to avoid to encounter such hard to track deep errors.
    +    (Reported by dolmen++)
    +
    +
     2.06 2014-07-01T10:01:44Z
    
         commit 8259d5eb28919bc766c8b500151d5be7e944b7f2

--- lib/Test/TCP.pm
+++ lib/Test/TCP.pm
@@ -2,7 +2,7 @@ package Test::TCP;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '2.06';
+our $VERSION = '2.07';
 use base qw/Exporter/;
 use IO::Socket::INET;
 use Test::SharedFork 0.12;
