26afcdf01041d3673a1e4b48665a84dde217e6ad
richard.leach <richard.leach@nccgroup.com>
2016-06-30 19:50:03 +0100

    Make Test::TCP::CheckPort able to pass hashref containing $host.

--- lib/Net/EmptyPort.pm
+++ lib/Net/EmptyPort.pm
@@ -124,7 +124,7 @@ sub wait_port {
     my $waiter = _make_waiter($max_wait);
 
     while ( $waiter->() ) {
-        if ($^O eq 'MSWin32' ? `$^X -MTest::TCP::CheckPort -echeck_port $port $proto` : check_port({ host => $host, port => $port, proto => $proto })) {
+        if ($^O eq 'MSWin32' ? `$^X -MTest::TCP::CheckPort -echeck_port $host $port $proto` : check_port({ host => $host, port => $port, proto => $proto })) {
             return 1;
         }
     }
